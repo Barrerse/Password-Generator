c56b0c6a16c10d572b0f4ae8c100ba1b8c3bebb1
Barrerse <sebastianbarrera@live.com>
2022-10-12 15:25:02 -0400

    added character variables and created first user input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,20 @@
 // Assignment code here
 
 
+function generatePassword() {
+  var numbArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var specialArray = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var possibleArray = [];
+
+  charInput = prompt ("How many characters do you want your password to be? Enter a number between 8 and 128");
+
+
+
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
