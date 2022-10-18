b94238bbc1a39d2ff59b769495b4cdd1f8802b5a
Barrerse <sebastianbarrera@live.com>
2022-10-17 20:55:42 -0400

    Completed the Java algorithim

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ function generatePassword() {
   var possibleArray = [];
 
   charInput = prompt ("How many characters do you want your password to be? Enter a number between 8 and 128");
-  if (charInput < 8 || carInput > 128){
+  if (charInput < 8 || charInput > 128){
     return "Invalid entry. Enter a number between 8 and 128.";
   }
   else if (isNaN(charInput)){
@@ -19,7 +19,67 @@ function generatePassword() {
     alert("Your generated password will be "+charInput+" characters long.");
   }
 
-}
+  hasLowercase = confirm("Do you want your password to include lowercase characters?");
+  if (hasLowercase) {
+    var turnToLowercase = alert("Your password will have lowercase characters.");
+  }
+  else {
+    alert("Your password will NOT have lowercase characters.");
+  }
+
+  hasUppercase = confirm("Do you want your password to include uppercase characters?");
+  if (hasUppercase) {
+    alert("Your password will have uppercase characters.");
+  }
+  else {
+    alert("Your password will NOT have uppercase characters.");
+  }
+
+  hasNumbers = confirm("Do you want your password to include numbers?");
+  if (hasNumbers) {
+    alert("Your password will have numbers.");
+  }
+  else {
+    alert("Your password will NOT have numbers.");
+  }
+
+  hasSpecial = confirm("Do you want your password to include special characters?");
+  if (hasSpecial) {
+    alert("Your password will have special characters.");
+  }
+  else {
+    alert("Your password will NOT have special characters.");
+  }
+
+  if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
+    return "Please select at least one character type.";
+  };
+
+
+  if (hasLowercase) {
+    possibleArray = possibleArray.concat(lowerArray);
+  }
+  if (hasUppercase) {
+    possibleArray = possibleArray.concat(upperArray);
+  }
+  if (hasNumbers) {
+    possibleArray = possibleArray.concat(charInput);
+  }
+  if (hasSpecial) {
+    possibleArray = possibleArray.concat(specialArray);
+  }
+
+
+  let finalPassword = ""
+  for (let i = 0; i < charInput; i++) {
+    let rng =[Math.floor(Math.random() * possibleArray.length)];
+    
+    finalPassword = finalPassword + possibleArray[rng];
+  }
+  return finalPassword;
+};
+
+
 
 
 // Get references to the #generate element
